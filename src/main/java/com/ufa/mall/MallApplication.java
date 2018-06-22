215474533f2f816f85453fd1576c0755e263271b
xieyuanzheng <121546683@qq.com>
2018-06-22 11:45:33 +0800

    change master 11:45

--- src/main/java/com/ufa/mall/MallApplication.java
+++ src/main/java/com/ufa/mall/MallApplication.java
@@ -2,11 +2,18 @@ package com.ufa.mall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MallApplication {
+public class MallApplication extends SpringBootServletInitializer{
 
-	public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MallApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception{
 		SpringApplication.run(MallApplication.class, args);
 	}
 }
